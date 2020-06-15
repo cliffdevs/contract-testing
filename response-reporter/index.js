7b668fbbc6eb27b82f682c05bbf50e5cc52ac518
Cliff Workman <cliffworkman@pop-os.localdomain>
2020-06-15 15:44:57 -0700

    Individual report writing is working.

--- response-reporter/index.js
+++ response-reporter/index.js
@@ -2,25 +2,20 @@ const { REPORT_DIR, PORT, TARGET_URI } = process.env;
 const Express = require("express");
 const app = Express();
 const proxy = require("express-http-proxy");
-const fs = require("fs");
+const writeFile = require("writefile");
+const path = require("path");
 const uuid = require("uuid").v4;
 
 app.use('*', proxy(TARGET_URI, {
-    userResHeaderDecorator(_headers, _request, _response, _proxyRequest, _proxyResponse) {
+    userResHeaderDecorator(headers, _request, _response, _proxyRequest, _proxyResponse) {
         const report = {
-            "openapi-cop-source-request": _proxyResponse.getHeader("openapi-cop-source-request"),
-            "openapi-cop-validation-result": _proxyResponse.getHeader("openapi-cop-validation-result"),
+            "openapi-cop-source-request": JSON.parse(headers["openapi-cop-source-request"]),
+            "openapi-cop-validation-result": JSON.parse(headers["openapi-cop-validation-result"]),
         };
 
-        fs.writeFile(`${REPORT_DIR}/${uuid()}`, JSON.stringify(report), err => {
-            if(err) {
-                console.error(err);
-            } else {
-                console.debug("Serialized report file");
-            }
-        });
+        writeFile(path.join(REPORT_DIR, `${uuid()}.json`), JSON.stringify(report));
 
-        return _headers;
+        return headers;
     }
   }));
 
--- response-reporter/run.sh
+++ response-reporter/run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-rm -rf /project/build/reports/contract-validation
-mkdir /project/build/reports/contract-validation
+rm /project/build/reports/contract-validation/*
 
 npm install
 node index
